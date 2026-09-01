55d745776f1733df6486b273605ff8fd5b8bad62
EdwardElikplim <nmtbtv7bd8@privaterelay.appleid.com>
2026-09-01 00:49:21 +0000

    objectTransform

---
+++ objectTransform.js
@@ -0,0 +1,39 @@
+function isValidPerson(person){
+    if (person === null){
+        return false;
+    }
+    if (Array.isArray(person)){
+        return false;
+    }
+    if (typeof person !== "object"){
+        return false;
+    }
+    if (!person.firstName || !person.lastName){
+        return false;
+    }
+    return true;
+}
+function fullName(person){
+    if (!isValidPerson(person)){
+        return null;
+    }
+    return person.firstName.trim() + " " + person.lastName.trim();
+}
+function isAdult(person) {
+    if (!isValidPerson(person)){
+        return{
+            granted: false,
+            message: "invalid person"
+        };
+    }
+    if (person.age >= person.minAge){
+        return{
+            granted: true,
+            message: "Adult"
+        };
+    }
+    return {
+        granted: false,
+        message: "Not an adult"
+    };
+}
